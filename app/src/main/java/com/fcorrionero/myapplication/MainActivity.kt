cf5389138f827d99cc6d4d7f810f712864838811
fcorrionero <fcorrionero@gmail.com>
2022-06-01 17:06:32 +0200

    generate pdf file

--- app/src/main/java/com/fcorrionero/myapplication/MainActivity.kt
+++ app/src/main/java/com/fcorrionero/myapplication/MainActivity.kt
@@ -1,15 +1,40 @@
 package com.fcorrionero.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.graphics.pdf.PdfDocument
 import android.os.Bundle
+import android.os.Environment
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.github.gcacace.signaturepad.views.SignaturePad
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    val pageHeight = 1120
+    val pagewidth = 792
+    private val PERMISSION_REQUEST_CODE = 200
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (checkPermission()) {
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        } else {
+            requestPermission();
+        }
     }
 
     fun onSaveBtnClick(v: View) {
@@ -18,6 +43,48 @@ class MainActivity : AppCompatActivity() {
         val surname = this.findViewById<EditText>(R.id.textSurname)
         val signature = this.findViewById<SignaturePad>(R.id.signature_pad)
         val bitmap = signature.getTransparentSignatureBitmap()
-        val a = 1
+
+        val pdfDocument = PdfDocument()
+        val text = Paint()
+        val paint = Paint()
+
+        val pageInfo = PdfDocument.PageInfo.Builder(pagewidth, pageHeight, 1).create()
+        val myPage = pdfDocument.startPage(pageInfo);
+        text.typeface = Typeface.create(Typeface.DEFAULT, Typeface.NORMAL)
+        text.textSize = 15F
+        text.color = ContextCompat.getColor(this, R.color.purple_200)
+        text.textAlign = Paint.Align.CENTER
+
+        // creating a variable for canvas
+        // from our page of PDF.
+        // creating a variable for canvas
+        // from our page of PDF.
+        val canvas: Canvas = myPage.canvas
+
+
+        val scaledbmp = Bitmap.createScaledBitmap(bitmap, 140, 140, false)
+        canvas.drawBitmap(scaledbmp, 56F, 40F, paint)
+        canvas.drawText("This is sample document which we have created.", 396F, 560F, text);
+
+        pdfDocument.finishPage(myPage)
+
+        val file = File(Environment.getExternalStorageDirectory(), "GFG.pdf")
+        try {
+            pdfDocument.writeTo(FileOutputStream(file))
+        }catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        pdfDocument.close()
+    }
+
+    private fun checkPermission(): Boolean  {
+        val permission1 = ContextCompat.checkSelfPermission(applicationContext, WRITE_EXTERNAL_STORAGE)
+        val permission2 = ContextCompat.checkSelfPermission(applicationContext, READ_EXTERNAL_STORAGE)
+        return permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
     }
 }
