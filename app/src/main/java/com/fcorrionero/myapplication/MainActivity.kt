69cccaeac8507b41e9f861723f5291c1974532bf
fcorrionero <fcorrionero@gmail.com>
2022-06-06 11:56:21 +0200

    add logo

--- app/src/main/java/com/fcorrionero/myapplication/MainActivity.kt
+++ app/src/main/java/com/fcorrionero/myapplication/MainActivity.kt
@@ -1,21 +1,15 @@
 package com.fcorrionero.myapplication
 
-import android.Manifest.permission.READ_EXTERNAL_STORAGE
-import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Typeface
+import android.graphics.*
+import android.graphics.drawable.Drawable
 import android.graphics.pdf.PdfDocument
 import android.os.Bundle
 import android.os.Environment
 import android.view.View
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import com.github.gcacace.signaturepad.views.SignaturePad
 import java.io.File
 import java.io.FileOutputStream
@@ -36,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         val phone = this.findViewById<EditText>(R.id.editTextPhone)
         val name = this.findViewById<EditText>(R.id.textName)
         val surname = this.findViewById<EditText>(R.id.textSurname)
+        val details = this.findViewById<EditText>(R.id.editTextDetails)
         val signature = this.findViewById<SignaturePad>(R.id.signature_pad)
         val bitmap = signature.getTransparentSignatureBitmap()
 
@@ -47,26 +42,41 @@ class MainActivity : AppCompatActivity() {
         val myPage = pdfDocument.startPage(pageInfo)
         text.typeface = Typeface.create(Typeface.DEFAULT, Typeface.NORMAL)
         text.textSize = 15F
-        text.color = ContextCompat.getColor(this, R.color.purple_200)
-        text.textAlign = Paint.Align.CENTER
+        text.color = ContextCompat.getColor(this, R.color.black)
+        text.textAlign = Paint.Align.LEFT
 
-        // creating a variable for canvas
-        // from our page of PDF.
-        // creating a variable for canvas
-        // from our page of PDF.
         val canvas: Canvas = myPage.canvas
 
+        val logo: Bitmap? = BitmapFactory.decodeResource(resources, R.drawable.logo_appinformatica)
+        // ResourcesCompat.getDrawable(resources, R.drawable.logo_appinformatica, null)
 
-        val scaledbmp = Bitmap.createScaledBitmap(bitmap, 140, 140, false)
-        canvas.drawBitmap(scaledbmp, 56F, 40F, paint)
-        canvas.drawText("This is sample document which we have created.", 396F, 560F, text)
+        if (null != logo) {
+            canvas.drawBitmap(
+                Bitmap.createScaledBitmap(logo, logo.width / 5, logo.height / 5, false),
+                30F,
+                30F,
+                paint
+            )
+        }
+        canvas.drawText(
+            "${phone.text} ${name.text} ${surname.text}\n${details.text}",
+            56F,
+            120F,
+            text
+        )
+
+        val scaledBmp = Bitmap.createScaledBitmap(bitmap, 140, 140, false)
+        canvas.drawBitmap(scaledBmp, 56F, 350F, paint)
 
         pdfDocument.finishPage(myPage)
 
-        val file = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "GFG.pdf")
+        val file = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+            "GFG.pdf"
+        )
         try {
             pdfDocument.writeTo(FileOutputStream(file))
-        }catch (e: IOException) {
+        } catch (e: IOException) {
             e.printStackTrace()
         }
 
