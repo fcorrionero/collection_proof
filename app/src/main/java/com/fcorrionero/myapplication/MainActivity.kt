7367016082fd4c9dd55db7e8370e9e0bcceaca2d
fcorrionero <fcorrionero@gmail.com>
2022-06-07 16:22:07 +0200

    draw pdf page

--- app/src/main/java/com/fcorrionero/myapplication/MainActivity.kt
+++ app/src/main/java/com/fcorrionero/myapplication/MainActivity.kt
@@ -13,6 +13,9 @@ import com.github.gcacace.signaturepad.views.SignaturePad
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +23,8 @@ class MainActivity : AppCompatActivity() {
     val pagewidth = 792
     private val INITIAL_PAGE_HEIGHT = 60F
     private val LEFT_PAGE_MARGIN = 30F
+    private val LABEL_WIDTH_SPACING = 11F
+    private val LABEL_HEIGHT_SPACING = 1.2F
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,6 +56,14 @@ class MainActivity : AppCompatActivity() {
         drawLogo(canvas, paint, myPage)
         height = drawBillTitle(canvas, height)
         height = drawAENumber(canvas, height, defaultText)
+        val labelText = TextPaint()
+        labelText.typeface = Typeface.create(Typeface.DEFAULT, Typeface.BOLD)
+        labelText.textSize = 15F
+        labelText.color = ContextCompat.getColor(this, R.color.black)
+        labelText.textAlign = Paint.Align.LEFT
+        height = drawDeviceEnterDate(canvas, labelText, defaultText, height)
+        drawDeviceData(canvas, labelText, defaultText, height)
+        height = drawClientData(canvas, labelText, defaultText, height)
 
 //        height += defaultText.textSize*3
 //        canvas.drawText(
@@ -79,13 +92,117 @@ class MainActivity : AppCompatActivity() {
         pdfDocument.close()
     }
 
+    private fun drawDeviceData(
+        canvas: Canvas,
+        labelText: TextPaint,
+        dataText: TextPaint,
+        height: Float
+    ): Float {
+        val width = 370F
+        val dataWidth = width + getString(R.string.device_accessories).length*LABEL_WIDTH_SPACING
+        canvas.drawText(getString(R.string.device), width + 20F, height, labelText)
+        var tempHeight = height + labelText.textSize*LABEL_HEIGHT_SPACING
+        canvas.drawText(
+            getString(R.string.device_accessories),
+            width,
+            tempHeight,
+            labelText
+        )
+        canvas.drawText(
+            getString(R.string.device_accessories),
+            dataWidth,
+            tempHeight,
+            dataText
+        )
+        return height
+    }
+
+    private fun drawClientData(
+        canvas: Canvas,
+        labelText: TextPaint,
+        dataText: TextPaint,
+        height: Float
+    ): Float {
+        var tempHeight = height + labelText.textSize*LABEL_HEIGHT_SPACING
+        canvas.drawText(getString(R.string.client_data), 50F, height, labelText)
+        canvas.drawText(
+            getString(R.string.client_name),
+            LEFT_PAGE_MARGIN,
+            tempHeight,
+            labelText
+        )
+        canvas.drawText(
+            getString(R.string.client_name),
+            LEFT_PAGE_MARGIN + getString(R.string.client_name).length * LABEL_WIDTH_SPACING,
+            tempHeight,
+            dataText
+        )
+        tempHeight += labelText.textSize*LABEL_HEIGHT_SPACING
+        canvas.drawText(
+            getString(R.string.client_phone),
+            LEFT_PAGE_MARGIN,
+            tempHeight,
+            labelText
+        )
+        canvas.drawText(
+            getString(R.string.client_phone),
+            LEFT_PAGE_MARGIN + getString(R.string.client_name).length * LABEL_WIDTH_SPACING,
+            tempHeight,
+            dataText
+        )
+        tempHeight += labelText.textSize*LABEL_HEIGHT_SPACING
+        canvas.drawText(
+            getString(R.string.client_address),
+            LEFT_PAGE_MARGIN,
+            tempHeight,
+            labelText
+        )
+        canvas.drawText(
+            getString(R.string.client_address),
+            LEFT_PAGE_MARGIN + getString(R.string.client_name).length * LABEL_WIDTH_SPACING,
+            tempHeight,
+            dataText
+        )
+        tempHeight += labelText.textSize*LABEL_HEIGHT_SPACING
+        canvas.drawText(
+            getString(R.string.client_identification_number),
+            LEFT_PAGE_MARGIN,
+            tempHeight,
+            labelText
+        )
+        canvas.drawText(
+            getString(R.string.client_identification_number),
+            LEFT_PAGE_MARGIN + getString(R.string.client_name).length * LABEL_WIDTH_SPACING,
+            tempHeight,
+            dataText
+        )
+        tempHeight += labelText.textSize*LABEL_HEIGHT_SPACING
+
+        return tempHeight
+    }
+
+    private fun drawDeviceEnterDate(
+        canvas: Canvas,
+        labelText: TextPaint,
+        dataText: TextPaint,
+        height: Float
+    ): Float {
+        val simpleDateFormat = SimpleDateFormat("mm/dd/yyyy", Locale.ENGLISH)
+        val date = simpleDateFormat.format(Date.from(Instant.now()))
+        val width = 250F + getString(R.string.bill_enter_date).length * 8F
+        canvas.drawText(getString(R.string.bill_enter_date), 250F, height, labelText)
+        canvas.drawText(date, width, height, dataText)
+
+        return height + labelText.textSize + 15F
+    }
+
     private fun drawAENumber(
         canvas: Canvas,
         height: Float,
         text: TextPaint
     ): Float {
         canvas.drawText(getString(R.string.ae_number), LEFT_PAGE_MARGIN, height, text)
-        return height + text.fontSpacing
+        return height
     }
 
     private fun drawBillTitle(
@@ -97,8 +214,13 @@ class MainActivity : AppCompatActivity() {
         titleText.textSize = 30F
         titleText.color = ContextCompat.getColor(this, R.color.black)
         titleText.textAlign = Paint.Align.LEFT
-        canvas.drawText(getString(R.string.bill_title), LEFT_PAGE_MARGIN, height + titleText.fontSpacing + 30F, titleText)
-        return  (height + titleText.fontSpacing + 60F)
+        canvas.drawText(
+            getString(R.string.bill_title),
+            LEFT_PAGE_MARGIN,
+            height + titleText.fontSpacing + 30F,
+            titleText
+        )
+        return (height + titleText.fontSpacing + 60F)
     }
 
     private fun drawLogo(
